0d5089899c1720e8f23bd17c72fc32aef685c536
keeprok <140785214+keeprok@users.noreply.github.com>
2024-05-26 02:42:30 +0900

    renad : nomad3완

--- Task6/learn-nextjs14/components/movie-videos.tsx
+++ Task6/learn-nextjs14/components/movie-videos.tsx
@@ -1,9 +1,11 @@
+import { error } from 'console';
 import { API_URL } from '../app/(home)/page';
 
 async function getVideos(id: string) {
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
+  //   const response = await fetch(`${API_URL}/${id}/videos`);
+  //   return response.json();
+  throw new Error('something broke...');
 }
 
 export default async function MovieVideo({ id }: { id: string }) {
