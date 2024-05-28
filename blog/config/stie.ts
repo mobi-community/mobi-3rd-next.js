37d97b45f56f761f032afdb62be10ae2b6a9198b
snkchan <144839872+snkchan@users.noreply.github.com>
2024-05-28 15:48:27 +0900

    feat : site header

---
+++ blog/config/stie.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "Chan's Log",
+  url: "https://example.com",
+  description: "Test.testsets",
+  author: "Chan",
+  link: {
+    github: "https://github.com/snkchan",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
